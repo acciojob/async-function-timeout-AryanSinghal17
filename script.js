e82d72ebeed097d0c164ab5c20ee7acf0b2d1ed6
Aryan Singhal <115396133+AryanSinghal17@users.noreply.github.com>
2026-09-22 22:04:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-//your JS code here. If required.
+//your JS code here. If requiredconst intpuy=
+
+let btn = document.getElementById("btn");
+let num = document.getElementById("delay");
+let text = document.getElementById("text");
+let output = document.getElementById("output");
+
+const pro1 = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(text.value);
+    }, num.value*1000);
+  });
+};
+btn.addEventListener("click", () => {
+  async function fun() {
+    let val = await pro1();
+  output.innerHTML = `
+  <h3>${val}</h3>
+  `
+  }
+  fun();
+});
